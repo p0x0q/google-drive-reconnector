ee994e4d749eb64536ec54da0dc9c1aa21b3e490
p0x0q <p0x0q_jp@gmail.com>
2020-04-27 20:27:44 +0900

    some

--- google-drive-reconnector.py
+++ google-drive-reconnector.py
@@ -16,7 +16,7 @@ print(args)
 if args.google_drive_dir:
     mount_dir = str(args.google_drive_dir)
     if not os.path.exists(mount_dir):
-        print("{} is not available.")
+        print("{} is not available.".format(mount_dir))
         print("umount -d {}".format(mount_dir))
         subprocess.run("umount -d {}".format(mount_dir), shell=True)
         print("mount -a")
