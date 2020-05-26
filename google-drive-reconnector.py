269dc1cc50717277c8e5c2835715f7084330e023
p0x0q <p0x0q_jp@gmail.com>
2020-05-27 04:42:49 +0900

    add chk_flag1(make a test file, then delete file)

--- google-drive-reconnector.py
+++ google-drive-reconnector.py
@@ -2,6 +2,11 @@ import os
 import argparse
 import subprocess
 
+import random,string
+def randomname(n):
+    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+    return ''.join(randlst)
+
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
@@ -14,7 +19,23 @@ args = parser.parse_args()
 print(args)
 if args.google_drive_dir:
     mount_dir = str(args.google_drive_dir)
-    if not os.path.exists(mount_dir):
+    chk_flag1 = True
+    r_text = randomname(12)
+    try:
+        filepath = "{}/.google-drive-reconnector-fuse_{}".format(mount_dir,r_text)
+        fw = open(filepath,"fw",encoding='utf-8')
+        fw.write("1")
+        fw.close()
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        else:
+            chk_flag1 = False
+    except:
+        chk_flag1 = False
+        print("Error, chk_flag1 = False")
+        import traceback
+        traceback.print_exc()
+    if not os.path.exists(mount_dir) and chk_flag1 == False:
         print("{} is not available.".format(mount_dir))
         print("umount -d {}".format(mount_dir))
         subprocess.run("umount -d {}".format(mount_dir), shell=True)
