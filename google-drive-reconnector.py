3afa4c51e20ee69f50b9e26b28fdf28f629a2e0f
p0x0q <p0x0q_jp@gmail.com>
2020-05-27 04:44:16 +0900

    Revert "add chk_flag1(make a test file, then delete file)"
    This reverts commit 269dc1cc50717277c8e5c2835715f7084330e023.

--- google-drive-reconnector.py
+++ google-drive-reconnector.py
@@ -2,11 +2,6 @@ import os
 import argparse
 import subprocess
 
-import random,string
-def randomname(n):
-    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
-    return ''.join(randlst)
-
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
@@ -20,23 +15,7 @@ args = parser.parse_args()
 print(args)
 if args.google_drive_dir:
     mount_dir = str(args.google_drive_dir)
-    chk_flag1 = True
-    r_text = randomname(12)
-    try:
-        filepath = "{}/.google-drive-reconnector-fuse_{}".format(mount_dir,r_text)
-        fw = open(filepath,"fw",encoding='utf-8')
-        fw.write("1")
-        fw.close()
-        if os.path.exists(filepath):
-            os.remove(filepath)
-        else:
-            chk_flag1 = False
-    except:
-        chk_flag1 = False
-        print("Error, chk_flag1 = False")
-        import traceback
-        traceback.print_exc()
-    if not os.path.exists(mount_dir) and chk_flag1 == False:
+    if not os.path.exists(mount_dir):
         print("{} is not available.".format(mount_dir))
         print("umount -d {}".format(mount_dir))
         subprocess.run("umount -d {}".format(mount_dir), shell=True)
