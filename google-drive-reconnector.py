0856391f0ffa57ac2db43220a23addfa87fe294f
p0x0q <p0x0q_jp@gmail.com>
2020-06-23 18:25:01 +0900

    remount new algo

--- google-drive-reconnector.py
+++ google-drive-reconnector.py
@@ -16,10 +16,12 @@ if args.google_drive_dir:
     mount_dir = str(args.google_drive_dir)
     if not os.path.exists(mount_dir):
         print("{} is not available.".format(mount_dir))
-        print("umount -d {}".format(mount_dir))
         subprocess.run("umount -d {}".format(mount_dir), shell=True)
         subprocess.run("fusermount -u  {}".format(mount_dir), shell=True)
-        print("mount -a")
+        subprocess.run("fusermount -l  {}".format(mount_dir), shell=True)
         subprocess.run("mount -a", shell=True)
+        #むやみにアップデートするとクラッシュする
+        #subprocess.run("su - google", shell=True)
+        #subprocess.run("opam upgrade -y google-drive-ocamlfuse", shell=True)
     else:
         print("{} is available.".format(mount_dir))
